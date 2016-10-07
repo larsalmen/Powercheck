8035ff7a11a1fcc752555738c8530fa993cd2f44
larsalmen <lars@sannagatan.se>
2016-10-07 19:32:48 +0200

    Powercheck main folder added
    Don't really know how this works yet, so I just uploaded the entire folder.

---
+++ Powercheck/Powercheck/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace Powercheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxHost = new System.Windows.Forms.TextBox();
+            this.labelIpAdress = new System.Windows.Forms.Label();
+            this.numericUpDownKeepAlive = new System.Windows.Forms.NumericUpDown();
+            this.labelKeepAlive = new System.Windows.Forms.Label();
+            this.numericUpDownTries = new System.Windows.Forms.NumericUpDown();
+            this.labelNumberofTries = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.numericUpDownPingTimer = new System.Windows.Forms.NumericUpDown();
+            this.labelPingTimer = new System.Windows.Forms.Label();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.checkBoxStartStopp = new System.Windows.Forms.CheckBox();
+            this.labelPingResult = new System.Windows.Forms.Label();
+            this.labelPingFail = new System.Windows.Forms.Label();
+            this.panelNumeric = new System.Windows.Forms.Panel();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.systemTrayicon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStripTrayicon = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abortShutdownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownKeepAlive)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTries)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPingTimer)).BeginInit();
+            this.panelNumeric.SuspendLayout();
+            this.contextMenuStripTrayicon.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxHost
+            // 
+            this.textBoxHost.Location = new System.Drawing.Point(108, 14);
+            this.textBoxHost.Name = "textBoxHost";
+            this.textBoxHost.Size = new System.Drawing.Size(273, 22);
+            this.textBoxHost.TabIndex = 0;
+            // 
+            // labelIpAdress
+            // 
+            this.labelIpAdress.AutoSize = true;
+            this.labelIpAdress.Location = new System.Drawing.Point(12, 17);
+            this.labelIpAdress.Name = "labelIpAdress";
+            this.labelIpAdress.Size = new System.Drawing.Size(88, 17);
+            this.labelIpAdress.TabIndex = 1;
+            this.labelIpAdress.Text = "Host to ping:";
+            // 
+            // numericUpDownKeepAlive
+            // 
+            this.numericUpDownKeepAlive.Location = new System.Drawing.Point(19, 11);
+            this.numericUpDownKeepAlive.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownKeepAlive.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownKeepAlive.Name = "numericUpDownKeepAlive";
+            this.numericUpDownKeepAlive.Size = new System.Drawing.Size(70, 22);
+            this.numericUpDownKeepAlive.TabIndex = 2;
+            this.numericUpDownKeepAlive.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // labelKeepAlive
+            // 
+            this.labelKeepAlive.AutoSize = true;
+            this.labelKeepAlive.Location = new System.Drawing.Point(14, 86);
+            this.labelKeepAlive.Name = "labelKeepAlive";
+            this.labelKeepAlive.Size = new System.Drawing.Size(253, 17);
+            this.labelKeepAlive.TabIndex = 3;
+            this.labelKeepAlive.Text = "Shutdown after power failure (1-5min): ";
+            // 
+            // numericUpDownTries
+            // 
+            this.numericUpDownTries.Location = new System.Drawing.Point(19, 39);
+            this.numericUpDownTries.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownTries.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownTries.Name = "numericUpDownTries";
+            this.numericUpDownTries.Size = new System.Drawing.Size(70, 22);
+            this.numericUpDownTries.TabIndex = 4;
+            this.numericUpDownTries.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // labelNumberofTries
+            // 
+            this.labelNumberofTries.AutoSize = true;
+            this.labelNumberofTries.Location = new System.Drawing.Point(14, 114);
+            this.labelNumberofTries.Name = "labelNumberofTries";
+            this.labelNumberofTries.Size = new System.Drawing.Size(187, 17);
+            this.labelNumberofTries.TabIndex = 5;
+            this.labelNumberofTries.Text = "Allowable ping failures (1-5):";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(15, 172);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(70, 30);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.Text = "Save...";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // numericUpDownPingTimer
+            // 
+            this.numericUpDownPingTimer.Location = new System.Drawing.Point(19, 67);
+            this.numericUpDownPingTimer.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownPingTimer.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownPingTimer.Name = "numericUpDownPingTimer";
+            this.numericUpDownPingTimer.Size = new System.Drawing.Size(70, 22);
+            this.numericUpDownPingTimer.TabIndex = 9;
+            this.numericUpDownPingTimer.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // labelPingTimer
+            // 
+            this.labelPingTimer.AutoSize = true;
+            this.labelPingTimer.Location = new System.Drawing.Point(14, 142);
+            this.labelPingTimer.Name = "labelPingTimer";
+            this.labelPingTimer.Size = new System.Drawing.Size(231, 17);
+            this.labelPingTimer.TabIndex = 10;
+            this.labelPingTimer.Text = "Minutes between checks (1-10min):";
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(15, 43);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(70, 30);
+            this.buttonTest.TabIndex = 11;
+            this.buttonTest.Text = "Test";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // checkBoxStartStopp
+            // 
+            this.checkBoxStartStopp.AutoSize = true;
+            this.checkBoxStartStopp.Location = new System.Drawing.Point(280, 173);
+            this.checkBoxStartStopp.Name = "checkBoxStartStopp";
+            this.checkBoxStartStopp.Size = new System.Drawing.Size(101, 21);
+            this.checkBoxStartStopp.TabIndex = 12;
+            this.checkBoxStartStopp.Text = "Start/Stopp";
+            this.checkBoxStartStopp.UseVisualStyleBackColor = true;
+            this.checkBoxStartStopp.CheckStateChanged += new System.EventHandler(this.checkBoxStartStopp_CheckStateChanged);
+            // 
+            // labelPingResult
+            // 
+            this.labelPingResult.AutoSize = true;
+            this.labelPingResult.Location = new System.Drawing.Point(110, 51);
+            this.labelPingResult.Name = "labelPingResult";
+            this.labelPingResult.Size = new System.Drawing.Size(0, 17);
+            this.labelPingResult.TabIndex = 13;
+            // 
+            // labelPingFail
+            // 
+            this.labelPingFail.AutoSize = true;
+            this.labelPingFail.Location = new System.Drawing.Point(269, 51);
+            this.labelPingFail.Name = "labelPingFail";
+            this.labelPingFail.Size = new System.Drawing.Size(0, 17);
+            this.labelPingFail.TabIndex = 14;
+            // 
+            // panelNumeric
+            // 
+            this.panelNumeric.Controls.Add(this.numericUpDownKeepAlive);
+            this.panelNumeric.Controls.Add(this.numericUpDownTries);
+            this.panelNumeric.Controls.Add(this.numericUpDownPingTimer);
+            this.panelNumeric.Location = new System.Drawing.Point(290, 72);
+            this.panelNumeric.Name = "panelNumeric";
+            this.panelNumeric.Size = new System.Drawing.Size(94, 97);
+            this.panelNumeric.TabIndex = 15;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(91, 172);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(128, 30);
+            this.buttonCancel.TabIndex = 16;
+            this.buttonCancel.Text = "Abort shutdown";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // systemTrayicon
+            // 
+            this.systemTrayicon.ContextMenuStrip = this.contextMenuStripTrayicon;
+            this.systemTrayicon.Icon = ((System.Drawing.Icon)(resources.GetObject("systemTrayicon.Icon")));
+            this.systemTrayicon.Text = "Powercheck";
+            this.systemTrayicon.Visible = true;
+            this.systemTrayicon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.systemTrayicon_MouseDoubleClick);
+            // 
+            // contextMenuStripTrayicon
+            // 
+            this.contextMenuStripTrayicon.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStripTrayicon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.abortShutdownToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenuStripTrayicon.Name = "contextMenuStripTrayicon";
+            this.contextMenuStripTrayicon.Size = new System.Drawing.Size(191, 136);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(190, 26);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(190, 26);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // abortShutdownToolStripMenuItem
+            // 
+            this.abortShutdownToolStripMenuItem.Name = "abortShutdownToolStripMenuItem";
+            this.abortShutdownToolStripMenuItem.Size = new System.Drawing.Size(190, 26);
+            this.abortShutdownToolStripMenuItem.Text = "Abort shutdown";
+            this.abortShutdownToolStripMenuItem.Click += new System.EventHandler(this.abortShutdownToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(190, 26);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 213);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.textBoxHost);
+            this.Controls.Add(this.panelNumeric);
+            this.Controls.Add(this.labelPingFail);
+            this.Controls.Add(this.labelPingResult);
+            this.Controls.Add(this.checkBoxStartStopp);
+            this.Controls.Add(this.buttonTest);
+            this.Controls.Add(this.labelPingTimer);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.labelNumberofTries);
+            this.Controls.Add(this.labelKeepAlive);
+            this.Controls.Add(this.labelIpAdress);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(415, 260);
+            this.MinimumSize = new System.Drawing.Size(415, 260);
+            this.Name = "Form1";
+            this.Text = "Powercheck";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownKeepAlive)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTries)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPingTimer)).EndInit();
+            this.panelNumeric.ResumeLayout(false);
+            this.contextMenuStripTrayicon.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxHost;
+        private System.Windows.Forms.Label labelIpAdress;
+        private System.Windows.Forms.NumericUpDown numericUpDownKeepAlive;
+        private System.Windows.Forms.Label labelKeepAlive;
+        private System.Windows.Forms.NumericUpDown numericUpDownTries;
+        private System.Windows.Forms.Label labelNumberofTries;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.NumericUpDown numericUpDownPingTimer;
+        private System.Windows.Forms.Label labelPingTimer;
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.CheckBox checkBoxStartStopp;
+        private System.Windows.Forms.Label labelPingResult;
+        private System.Windows.Forms.Label labelPingFail;
+        private System.Windows.Forms.Panel panelNumeric;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.NotifyIcon systemTrayicon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripTrayicon;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abortShutdownToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    }
+}
+
---
+++ Powercheck/Powercheck/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Net.NetworkInformation;
+using System.Timers;
+
+namespace Powercheck
+{
+
+    /// <summary>
+    /// TODO: Encapsulate all variables with props. DRY it up, some methods are extremely similar.
+    /// Understand threading, not just work around it. Use backgroundworker or something for timer.
+    /// Add list of hosts to ping, with dropdown menu.
+    /// Add tooltip over trayicon that shows current status.
+    /// Log failures and exceptions. Validate hosts input...?
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        delegate void SetTextCallback(string text); // Delegate used to set textboxes when threading acts up...
+        int timeToShutdown; // Time in minutes until shutdown via windows logoff screen. Set via numericupdown.
+        int pingTimer; // Time in minutes between ping tries. Set via numericupdown.
+        string hostToPing; // Host to ping. Input from textbox.
+        int allowablePingFail; // Allowable tries before shutdown. Set via numericupdown.
+        int currentPingFail; // Current failures, increments every failed ping.
+        System.Timers.Timer ping; // Timer used to ping etc.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Sets numericupdowns, host textbox etc from Settings file.
+            // Starts timer.
+            UpdateFromSettings();
+
+            checkBoxStartStopp.Appearance = Appearance.Button;
+
+            ping = new System.Timers.Timer();
+            ping.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            ping.Interval = pingTimer;
+        }
+        private void UpdateSettings()
+        {
+            // Updates variables with values from controls.
+            timeToShutdown = 60 * Convert.ToInt32(numericUpDownKeepAlive.Value.ToString());
+            pingTimer = 60 * (Convert.ToInt32(numericUpDownPingTimer.Value.ToString()) * 1000);
+            allowablePingFail = Convert.ToInt32(numericUpDownTries.Value.ToString());
+            hostToPing = textBoxHost.Text;
+        }
+        private bool PingHost(string ipAdress)
+        {
+            // Pings host, calls to method to display result on main form and returns ping result as bool.
+            bool pingable = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(hostToPing);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+                // Should probably implement logging here.
+            }
+            DisplayPingResult(pingable);
+            return pingable;
+        }
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // Calls to method to update variables with current settings and method to save settings in settings file.
+            UpdateSettings();
+            SaveSettings();
+        }
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            // Does all the magic when the pingTimer has elapsed.
+            if (!PingHost(hostToPing))
+                currentPingFail++;
+            else
+                currentPingFail = 0;
+
+            string result = $"Ping failures: {currentPingFail}";
+            SetLabelPingFail(result);
+
+            if (currentPingFail > allowablePingFail)
+                Process.Start($"shutdown", $"/s /t {timeToShutdown}");
+        }
+        private void SaveSettings()
+        {
+            // Saves current settings to settings file.
+            Properties.Settings.Default.TimeToShutDownSetting = (int)numericUpDownKeepAlive.Value;
+            Properties.Settings.Default.PingTimerSetting = (int)numericUpDownPingTimer.Value;
+            Properties.Settings.Default.HostToPingSetting = hostToPing;
+            Properties.Settings.Default.AllowablePingFailSetting = (int)numericUpDownTries.Value;
+            Properties.Settings.Default.Save();
+        }
+        private void UpdateFromSettings()
+        {
+            // Update controls with values from current settings file.
+            timeToShutdown = 60 * Properties.Settings.Default.TimeToShutDownSetting;
+            pingTimer = (Properties.Settings.Default.PingTimerSetting * 1000) * 60;
+            hostToPing = Properties.Settings.Default.HostToPingSetting;
+            allowablePingFail = Properties.Settings.Default.AllowablePingFailSetting;
+
+            textBoxHost.Text = Properties.Settings.Default.HostToPingSetting;
+            numericUpDownKeepAlive.Value = Properties.Settings.Default.TimeToShutDownSetting;
+            numericUpDownPingTimer.Value = Properties.Settings.Default.PingTimerSetting;
+            numericUpDownTries.Value = Properties.Settings.Default.AllowablePingFailSetting;
+        }
+        private void Start_Stopp(bool start)
+        {
+            // Starts or stops the timer and enables/disables controls.
+            if (start)
+            {
+                ping.Enabled = true;
+                textBoxHost.Enabled = false;
+                panelNumeric.Enabled = false;
+            }
+            else
+            {
+                ping.Enabled = false;
+                textBoxHost.Enabled = true;
+                panelNumeric.Enabled = true;
+            }
+            currentPingFail = 0;
+        }
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            // Test to see if the current set host is pingable.
+            hostToPing = textBoxHost.Text;
+            DisplayPingResult(PingHost(hostToPing));
+        }
+        private void checkBoxStartStopp_CheckStateChanged(object sender, EventArgs e)
+        {
+            // Updates variables, clears labels and toggles timer.
+            UpdateSettings();
+            Start_Stopp(checkBoxStartStopp.Checked);
+            labelPingFail.Text = "";
+            labelPingResult.Text = "";
+        }
+        public void DisplayPingResult(bool pingable)
+        {
+            // Displays the result of the last attempt to ping host.
+            if (pingable)
+            {
+                string result = "Ping succesful.";
+                SetLabelPingResult(result);
+                labelPingResult.BackColor = Color.Green;
+            }
+            else
+            {
+                string result = "Ping unsuccesful.";
+                SetLabelPingResult(result);
+                labelPingResult.BackColor = Color.Red;
+            }
+        }
+        private void SetLabelPingResult(string result)
+        {
+            // Uses delagate to update labels on main form with the last ping results.
+            if (labelPingResult.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetLabelPingResult);
+                Invoke(d, new object[] { result });
+            }
+            else
+            {
+                labelPingResult.Text = result;
+            }
+        }
+        private void SetLabelPingFail(string result)
+        {
+            // Uses delagate to update labels on main form with the last ping results.
+
+            if (labelPingFail.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetLabelPingFail);
+                Invoke(d, new object[] { result });
+            }
+            else
+            {
+                labelPingFail.Text = result;
+            }
+        }
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            // Cancels current shutdown timer (if any) and stops the timer.
+            AbortShutdown();
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // Minimizes the window to system tray.
+            if (FormWindowState.Minimized == WindowState)
+            {
+                systemTrayicon.Visible = true;                
+                Hide();
+            }
+            else if (FormWindowState.Normal == WindowState)
+            {
+                systemTrayicon.Visible = false;
+            }
+        }
+        private void systemTrayicon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // Shows the main form and brings it to focus.
+            Show();
+            WindowState = FormWindowState.Normal;
+            Activate();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Start_Stopp(true);
+        }
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Start_Stopp(false);
+        }
+        private void abortShutdownToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AbortShutdown();
+        }
+        private void AbortShutdown()
+        {
+            Process.Start($"shutdown", $"/a");
+            Start_Stopp(false);
+        }
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
